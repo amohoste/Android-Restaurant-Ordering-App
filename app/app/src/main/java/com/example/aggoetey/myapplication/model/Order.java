57fb83183b6246accc1f11ea4a9ff6c5cbf376ca
Arne Goeteyn <goeteynarne@gmail.com>
2018-03-23 11:48:56 +0100

    add remove OrderItem to Order + made Observable

--- app/app/src/main/java/com/example/aggoetey/myapplication/model/Order.java
+++ app/app/src/main/java/com/example/aggoetey/myapplication/model/Order.java
@@ -2,13 +2,14 @@ package com.example.aggoetey.myapplication.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
 
 /**
  * Created by aggoetey on 3/20/18.
  *
  */
 
-public class Order {
+public class Order extends Observable {
     private List<OrderItem> orderItems = new ArrayList<>();
 
     public Order() {
@@ -20,5 +21,13 @@ public class Order {
 
     public void addOrderItem (OrderItem orderItem){
         this.orderItems.add(orderItem);
+        setChanged();
+        this.notifyObservers();
+    }
+
+    public void removeOrderItem(OrderItem orderItem){
+        this.orderItems.remove(orderItem);
+        setChanged();
+        this.notifyObservers();
     }
 }
