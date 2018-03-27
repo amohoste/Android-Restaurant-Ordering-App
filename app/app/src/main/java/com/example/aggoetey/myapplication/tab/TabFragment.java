9cc94c85729a4e485920a4ca42366f8bf92e2c52
Arne Goeteyn <goeteynarne@gmail.com>
2018-03-27 10:32:04 +0200

    orders worden verwijderd, enkel nog niet visueel aangepast

--- app/app/src/main/java/com/example/aggoetey/myapplication/tab/TabFragment.java
+++ app/app/src/main/java/com/example/aggoetey/myapplication/tab/TabFragment.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.aggoetey.myapplication.R;
@@ -21,6 +23,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public class TabFragment extends Fragment {
 
@@ -33,7 +36,10 @@ public class TabFragment extends Fragment {
                 new OrderItem("notitie", new MenuItem("spaghetti", 13, "tettne", "ca")),
                 new OrderItem("notitie", new MenuItem("lasagna", 15, "tettne", "ca"))
         )));
-
+        tab.addOrder(new Order(Arrays.asList(
+                new OrderItem("notitie", new MenuItem("spaghetti", 18, "tettne", "ca")),
+                new OrderItem("notitie", new MenuItem("lasagna", 15, "tettne", "ca"))
+        )));
         View view = inflater.inflate(R.layout.fragment_tab, container, false);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.tabRecyclerView);
 
@@ -52,7 +58,27 @@ public class TabFragment extends Fragment {
         TextView total = view.findViewById(R.id.total);
         total.setText(String.valueOf(prijs));
 
+        registerPayButtonListener((Button) view.findViewById(R.id.pay_button), tabAdapter);
+
         return view;
     }
 
+    private void registerPayButtonListener(Button button, final TabAdapter tabAdapter) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test", String.valueOf(TabSingleton.getInstance().getTab().getOrderedOrders().size()));
+                payOrders();
+                tabAdapter.notifyDataSetChanged();
+                Log.d("test", String.valueOf(TabSingleton.getInstance().getTab().getOrderedOrders().size()));
+            }
+        });
+    }
+
+    private void payOrders() {
+        List<Order> orderedOrders = new ArrayList<>(TabSingleton.getInstance().getTab().getOrderedOrders());
+            for (Order orderedOrder : orderedOrders) {
+                TabSingleton.getInstance().getTab().payOrder(orderedOrder);
+            }
+        }
 }
