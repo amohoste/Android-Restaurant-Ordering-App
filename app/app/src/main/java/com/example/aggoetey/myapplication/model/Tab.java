775501ddd2ee35760ee6eebbe9a4581be52d4102
Arne Goeteyn <goeteynarne@gmail.com>
2018-03-27 14:41:56 +0200

    tab en TabSingleton samen

--- app/app/src/main/java/com/example/aggoetey/myapplication/model/Tab.java
+++ app/app/src/main/java/com/example/aggoetey/myapplication/model/Tab.java
@@ -1,36 +1,45 @@
 package com.example.aggoetey.myapplication.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Observable;
-import java.util.Set;
 
 /**
  * Created by aggoetey on 3/20/18.
- *
+ * <p>
  * Een tab is een lijst met orders die nog betaald moeten worden door een bepaalde tafel.
  */
 
 public class Tab extends Observable {
-    private Set<Order> payedOrders = new HashSet<>();
-    private Set<Order> orderedOrders = new HashSet<>();
 
-    public Set<Order> getPayedOrders() {
+    private static final Tab ourInstance = new Tab();
+
+    private List<Order> payedOrders = new ArrayList<>();
+    private List<Order> orderedOrders = new ArrayList<>();
+
+    public List<Order> getPayedOrders() {
         return payedOrders;
     }
 
-    public Set<Order> getOrderedOrders() {
+    public List<Order> getOrderedOrders() {
         return orderedOrders;
     }
 
-    public void addOrder(Order order){
+    private Tab() {
+    }
+
+    static Tab getInstance() {
+        return ourInstance;
+    }
+
+
+    public void addOrder(Order order) {
         orderedOrders.add(order);
         this.setChanged();
         this.notifyObservers();
     }
 
-    public void payOrder(Order order){
+    public void payOrder(Order order) {
         orderedOrders.remove(order);
         payedOrders.add(order);
         this.setChanged();
