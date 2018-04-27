e0953c797ec4399074b1a2eabad54e8c6aa93c20
Arne Goeteyn <goeteynarne@gmail.com>
2018-04-27 12:41:01 +0200

    --amend

---
+++ app/app/src/main/java/com/example/aggoetey/myapplication/pay/orderdetail/OrderDetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.aggoetey.myapplication.pay.orderdetail;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.aggoetey.myapplication.R;
+import com.example.aggoetey.myapplication.model.Tab;
+
+import static com.example.aggoetey.myapplication.pay.orderdetail.OrderDetailFragment.ORDER_KEY;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        FragmentManager manager = getSupportFragmentManager();
+        OrderDetailFragment orderDetailFragment = OrderDetailFragment.newInstance((Tab.Order) getIntent().getSerializableExtra(ORDER_KEY));
+        manager.beginTransaction().replace(R.id.order_fragment, orderDetailFragment).commit();
+    }
+}
