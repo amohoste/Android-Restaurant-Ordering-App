7a2f7e712612498fac6d8b5961e87f8d1c65721a
Arne Goeteyn <goeteynarne@gmail.com>
2018-04-29 18:20:22 +0200

    afhandelen keuze betaalmethode

--- app/app/src/main/java/com/example/aggoetey/myapplication/pay/PayChoiceDialogFragment.java
+++ app/app/src/main/java/com/example/aggoetey/myapplication/pay/PayChoiceDialogFragment.java
@@ -5,19 +5,41 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 import com.example.aggoetey.myapplication.R;
 
 public class PayChoiceDialogFragment extends DialogFragment {
+
+    private PayChoiceListener payChoiceListener;
+
+    public static final String PAY_CHOICE = "PAY_CHOICE";
+
+    public interface PayChoiceListener {
+        void onPayChoiceSelection(int i);
+    }
+
+    @Override
+    public void onCreate(Bundle b) {
+        Log.d("test", "onCreate: ");
+        onAttachToParentFragment(getParentFragment());
+        super.onCreate(b);
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Betalingsmethode")
-                .setMessage("Op welke manier wenst u te betalen?")
-                .setItems(R.array.pay_options);
+        builder.setTitle(R.string.paychoicedialog_title)
+                .setItems(R.array.pay_options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        payChoiceListener.onPayChoiceSelection(i);
+                    }
+                });
         return builder.create();
     }
 
@@ -27,5 +49,14 @@ public class PayChoiceDialogFragment extends DialogFragment {
         PayChoiceDialogFragment fragment = new PayChoiceDialogFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public void onAttachToParentFragment(Fragment fragment) {
+        try {
+            payChoiceListener = (PayChoiceListener) fragment;
+        } catch (ClassCastException c) {
+            throw new ClassCastException(
+                    fragment.toString() + " must implement OnPlayerSelectionSetListener");
+        }
     }
 }
