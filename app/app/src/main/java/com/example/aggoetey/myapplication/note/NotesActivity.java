04b8f7ab0da577b269b4d7186232002fd43790b8
Sitt Min Oo <x.sittminoo@ugent.be>
2018-05-07 14:07:24 +0200

    init notes branch

---
+++ app/app/src/main/java/com/example/aggoetey/myapplication/note/NotesActivity.java
@@ -0,0 +1,16 @@
+package com.example.aggoetey.myapplication.note;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aggoetey.myapplication.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
