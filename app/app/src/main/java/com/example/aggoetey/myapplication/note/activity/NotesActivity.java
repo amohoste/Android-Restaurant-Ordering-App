747f60567e3320cd9dc67096769bd8f55ba0f26f
Sitt Min Oo <x.sittminoo@ugent.be>
2018-05-09 20:26:17 +0200

    start intent

--- app/app/src/main/java/com/example/aggoetey/myapplication/note/activity/NotesActivity.java
+++ app/app/src/main/java/com/example/aggoetey/myapplication/note/activity/NotesActivity.java
@@ -1,16 +1,23 @@
 package com.example.aggoetey.myapplication.note.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.aggoetey.myapplication.R;
+import com.example.aggoetey.myapplication.model.MenuInfo;
 
 public class NotesActivity extends AppCompatActivity {
 
+    public static  final String ARG_MENU_INFO = "ARG_MENU_INFO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        MenuInfo menuInfo =  (MenuInfo) intent.getSerializableExtra(ARG_MENU_INFO);
+
         setContentView(R.layout.activity_notes);
     }
 }
