89142c2c555f9061b60c5d5a4867dc4ba27f40e0
DriesMarzougui <dries.marzougui@ugent.be>
2018-05-13 14:32:47 +0200

    Prettier money format

--- app/app/src/main/java/com/example/aggoetey/myapplication/menu/fragments/MenuFragment.java
+++ app/app/src/main/java/com/example/aggoetey/myapplication/menu/fragments/MenuFragment.java
@@ -252,7 +252,8 @@ public class MenuFragment extends Fragment implements Listener, View.OnClickList
     public void setOrderButtonProperties() {
         if (Tab.getInstance().getTable().getTableId() != null) {
             if (menuInfo.getCurrentOrder().getOrderItems().size() > 0) {
-                mMenuOrderButton.setText(getResources().getString(R.string.menu_view_order_button) + " (€" + menuInfo.getCurrentOrder().getPrice() + ")");
+                mMenuOrderButton.setText(String.format("%s (€%.2f)",
+                        getResources().getString(R.string.menu_view_order_button), menuInfo.getCurrentOrder().getPrice()));
                 mMenuOrderButton.setEnabled(true);
             } else {
                 mMenuOrderButton.setText(getResources().getString(R.string.menu_view_order_button));
